d133ded255b01b26365c4b1403f1ae80c42f0d1a
Allen Jiang <alljiang@hotmail.com>
2020-02-01 12:26:57 -0600

    sdl2

---
+++ build/CMakeFiles/EE319K_Final_Project_Initial_Testing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EE319K_Final_Project_Initial_Testing.dir/main.cpp.obj"
+  "EE319K_Final_Project_Initial_Testing.pdb"
+  "EE319K_Final_Project_Initial_Testing.exe"
+  "EE319K_Final_Project_Initial_Testing.exe.manifest"
+  "libEE319K_Final_Project_Initial_Testing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EE319K_Final_Project_Initial_Testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <SDL.h>
+#include <stdlib.h>
 
-using namespace std;
+#define WINDOW_WIDTH 600
 
-int main() {
-    cout    << "Hello, World!" << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    int i;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawPoint(renderer, 50, 50);
+    SDL_RenderPresent(renderer);
+    while (1) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
