5e38324ef4c70b29f2f283b3efcd4b8f6c22b68c
Allen Jiang <alljiang@hotmail.com>
2020-02-03 17:18:38 -0600

    modified output format

--- data/sprites/SpriteParser/GIFParser.py
+++ data/sprites/SpriteParser/GIFParser.py
@@ -2,21 +2,48 @@ from PIL import Image, ImageSequence
 import numpy as np
 import os
 
+#   Format:
+#
+#   number of sequences
+#   {
+#       sequence name
+#       number of frames in sequence
+#       width of each frame (pixels)
+#       height of each frame (pixels)
+#       {
+#           (total number of colors in line) (color index) (number of repeated color) ...
+#       }
+#       ...
+#   }
+
+#   CONFIG
+backgroundColor = 0x00FF00
+characterName = "kirby"
+imageDirectory = "/gif-" + characterName + "/"
+
 # Output file
-output = open("data.txt", "w")
+output = open(characterName + ".txt", "w")
 
 indexed_rgb = []
 
-for filename in os.listdir(os.getcwd()):
+numFiles = len(os.listdir(os.getcwd() + "/gif-kirby/"))
+output.write(str(numFiles))
+for filename in os.listdir(os.getcwd() + "/gif-kirby/"):
     if filename.endswith(".gif") or filename.endswith(".png"):
-        img = Image.open(filename)
+        img = Image.open("./gif-kirby/" + filename)
         frames = np.array([np.array(frame.copy().convert('RGB').getdata(), dtype=np.uint8)
                           .reshape(frame.size[1], frame.size[0], 3) for frame in ImageSequence.Iterator(img)])
         compressed_rgb = []
+
+        frame_width = 0
+        frame_height = 0
+
         # Convert 8-bit color to 6-bit color and store in index array
         for frame in frames:
             compressed_frame = []
+            frame_height = len(frame)
             for row in frame:
+                frame_width = len(row);
                 compressed_row = []
                 prev_color = 0
                 line_len = 1
@@ -25,6 +52,8 @@ for filename in os.listdir(os.getcwd()):
                     green = int(pixel[1])
                     blue = int(pixel[2])
                     rgb_int = red << 16 | green << 8 | blue
+                    if rgb_int == backgroundColor:
+                        rgb_int = -1
                     if rgb_int == prev_color:
                         line_len += 1
                         if i == len(row)-1:
@@ -40,15 +69,17 @@ for filename in os.listdir(os.getcwd()):
             compressed_rgb.append(compressed_frame)
 
         # Write to file
-        output.write(f"{filename}: \n")
-        output.write(f"{len(frames)}\n")
+        output.write("\n")
+        output.write(filename.split(".")[0] + "\n")
+        output.write(str(len(frames)) + "\n")
+        output.write(str(frame_width) + "\n")
+        output.write(str(frame_height))
         for frame in compressed_rgb:
             for row in frame:
+                output.write("\n")
                 output.write(f"{len(row)}")
                 for line in row:
                     output.write(f" {line[0]} {line[1]}")
-                output.write("\n")
-            output.write("\n")
 output.write(f"Colors: \n {indexed_rgb}\n")
 
 
